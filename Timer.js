54a8cf2444cf450dd98869d37114cc0699b8b3af
nalinmanikandan <75604279+nalinmanikandan@users.noreply.github.com>
2023-07-01 01:30:11 +0530

    Add files via upload

---
+++ Timer.js
@@ -0,0 +1,8 @@
+let [milliseconds,seconds,minutes,hours]=[0,0,0,0];
+let timeRef = document.querySelector(".time");
+let int = null;
+document.getElementById("start").addEventListener("click",()=>{
+  setInterval(() => {
+    
+  }, 1);
+})
